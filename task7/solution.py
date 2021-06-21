4e809d3c919d001e9bfdf6d1bad856bdcc88f802
vkhalaim <vasyl.khalaim@ukr.net>
2021-06-21 16:10:08 +0300

    adds seventh task

---
+++ task7/solution.py
@@ -0,0 +1,8 @@
+def join_list_with_commas(list_of_strings):
+    """
+    Return joined string with commas of list elements.
+    :param list_of_strings:
+    :return: joined string of elements
+    """
+
+    return ",".join(list_of_strings)
---
+++ task7/tests.py
@@ -0,0 +1,21 @@
+import unittest
+
+from .solution import join_list_with_commas
+
+
+class TestJoinListWithCommas(unittest.TestCase):
+
+    def test_correct_list(self):
+        self.assertEqual(join_list_with_commas(["1", "2", "3"]), "1,2,3")
+        self.assertEqual(join_list_with_commas(["V", "K", "J"]), "V,K,J")
+        self.assertEqual(join_list_with_commas(["+", "-", "/"]), "+,-,/")
+        self.assertEqual(
+            join_list_with_commas(["my_string1", "my_string2", "my_string3"]),
+            "my_string1,my_string2,my_string3"
+        )
+
+    def test_empty_list(self):
+        self.assertEqual(join_list_with_commas([]), "")
+
+if __name__ == "__main__":
+    unittest.main()
